ea6e7ac6d20397aa2610f6ecca90122d39a4e5e4
fred wright <fredwright0@gmail.com>
2020-04-13 19:31:11 +0700

    chore: move react types to dependency

--- test/App.spec.tsx
+++ test/App.spec.tsx
@@ -2,8 +2,7 @@ import { render } from 'ink-testing-library'
 import React from 'react'
 import App from '../src/App'
 
-test.skip('renders', () => {
+test('renders', () => {
   const a = render(<App />)
   expect(a.lastFrame().includes('Create a new feature branch')).toBe(true)
-  a.unmount()
 })
