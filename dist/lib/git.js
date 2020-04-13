518bc804ccb05d4e3db52daac04df12911c2b3fa
fred wright <fredwright0@gmail.com>
2020-04-13 14:40:54 +0700

    chore: update semantic-release

--- dist/lib/git.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-exports.__esModule = true;
-var promise_1 = __importDefault(require("simple-git/promise"));
-var env_1 = __importDefault(require("./env"));
-var git = promise_1["default"](env_1["default"].rootDir);
-exports["default"] = git;
--- dist/lib/object.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-function isUndefined(x) {
-    return typeof x !== 'undefined' && x !== null;
-}
-exports.isUndefined = isUndefined;
