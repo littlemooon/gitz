1a41b050a4bfd69384abcecd84ea26ad8be8ebe9
fred wright <fredwright0@gmail.com>
2020-04-26 23:33:42 +0700

    fix: helptext

--- src/lib/cli.ts
+++ src/lib/cli.ts
@@ -3,8 +3,6 @@ import { CliCommand, cliCommands } from './command'
 import env from './env'
 
 export const cliHelpText = `
-gitz - interactive git client for feature workflow
-
 Usage:
   gitz                      index of commands
   gitz status               (s) current branch status
